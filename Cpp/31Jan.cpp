ab0ea2b0a1bf30f0f5705824ccb8a6b0889e55ad
Asia Splatty Ciafardini <110777081+neoreo-dev@users.noreply.github.com>
2026-01-31 09:16:15 +0100

    Add 31/01 stuff
    Generate random numbers given the array size. Includes variant with a preset 100-sized array.

---
+++ Cpp/31Jan.cpp
@@ -0,0 +1,35 @@
+/* 
+ * Data la dimensione dell'array, riempirlo con valori a caso e stampare tutti i valori.
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int n=0;
+    int array[100];
+    
+    while (n<=0 || n>100) {
+        cout << "Quanti numeri vuoi generare? ";
+        cin>>n;
+        if (n<=0 || n>100) {
+            cout<<"Non puoi inserire questo valore!"<<endl;
+        }
+    }
+    
+    int rNum=rand()%10;
+
+    cout<<"Ecco i "<<n<<" numeri che ho generato:"<<endl;
+
+    for (int i=0; i<n; i++) {
+        array[i]=rNum;
+        cout<<array[i]<<endl;
+        rNum=rand()%10;
+    }
+    
+    return 0;
+}
